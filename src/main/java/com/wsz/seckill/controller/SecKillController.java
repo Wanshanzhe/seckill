bfa9c151b987681e936b3cc6e2ee8ce49510e097
wuhao <1970458661@qq.com>
2022-11-27 18:05:56 +0800

    fix：秒杀方法小调整

--- src/main/java/com/wsz/seckill/controller/SecKillController.java
+++ src/main/java/com/wsz/seckill/controller/SecKillController.java
@@ -131,9 +131,10 @@ public class SecKillController implements InitializingBean {
             return RespBean.error(RespBeanEnum.EMPTY_STOCK);
         }
         //预减库存
-//        Long stock = valueOperations.decrement("seckillGoods:" + goodsId);
+        //通过内存标记减少redis的访问
         Long stock = (Long)redisTemplate.execute(script, Collections.singletonList("seckillGoods:" + goodsId),
                 Collections.EMPTY_LIST);
+
         if (stock < 0){
             EmptyStockMap.put(goodsId, true);
             valueOperations.increment("seckillGoods:" + goodsId);
